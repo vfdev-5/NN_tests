a0d688c2748d1ccc458ff8b66b33e5450d4e8929
uids3502 <uids3502@contiwan.com>
2017-06-30 17:04:46 +0200

    *[WIP] Up

--- tf_bike/train.py
+++ tf_bike/train.py
@@ -8,17 +8,17 @@ import tensorflow as tf
 
 class Trainer:
 
-    def __init__(self, log_dir="logs"):
+    def __init__(self, log_dir="logs", display_step=100):
         self._log_dir = log_dir
-        self.display_step = 100
+        self.display_step = display_step
 
     @staticmethod
-    def get_batch(batch_index, batch_size, train_x, train_y=None):
+    def get_batch(batch_index, batch_size, x, y=None):
         start = batch_size * batch_index
-        end = batch_size * (batch_index + 1)
-        if train_y:
-            return train_x[start:end, ...], train_y[start:end, ...], (start, end)
-        return train_x[start:end, ...], (start, end)
+        end = min(batch_size * (batch_index + 1), x.shape[0])
+        if y is not None:
+            return x[start:end, ...], y[start:end, ...], (start, end)
+        return x[start:end, ...], (start, end)
 
     @staticmethod
     def _check_params(training_params):
@@ -58,8 +58,8 @@ class Trainer:
         n_classes = np.prod(trainval_y.shape[1:])
 
         tf.reset_default_graph()
-        X = tf.placeholder(tf.float32, shape=(batch_size, n_features), name="X")
-        Y_true = tf.placeholder(tf.float32, shape=(batch_size, n_classes), name="Y_true")
+        X = tf.placeholder(tf.float32, shape=(None, n_features), name="X")
+        Y_true = tf.placeholder(tf.float32, shape=(None, n_classes), name="Y_true")
 
         with tf.name_scope('Model'):
             Y_pred = network_f(X)
@@ -117,8 +117,6 @@ class Trainer:
         if 'pretrained_model' not in training_params:
             # find the best checkpoint
             pretrained_model_path = find_best_checkpoint(os.path.join(self._log_dir, "Model"))
-            if verbose > 0 and pretrained_model_path is not None:
-                print("Load found pretrained model: %s" % pretrained_model_path)
         else:
             pretrained_model_path = training_params['pretrained_model']
 
@@ -128,6 +126,10 @@ class Trainer:
         init_fn = None if pretrained_model_path is None else load_pretrained
 
         vars_to_store = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Model')
+        if verbose > 0:
+            n_params = np.sum([np.product([xi.value for xi in x.get_shape()]) for x in vars_to_store])
+            print("Model contains %i of trainable variables" % len(vars_to_store), end=' ')
+            print("and %i of trainable parameters" % int(n_params))
         vars_to_store.append(global_step)
         saver = tf.train.Saver(vars_to_store, max_to_keep=20)
         sv = tf.train.Supervisor(logdir=train_path,
@@ -158,33 +160,45 @@ class Trainer:
             def run_epoch(ops, summary_op, _x, _y, _writer, is_training_phase):
                 avg_loss = 0.0
                 avg_metrics = np.zeros((len(metrics),))
-                n_batchs = int(_x.shape[0] / batch_size)
+                n_samples = _x.shape[0]
+                n_batchs = int(np.ceil(n_samples * 1.0 / batch_size))
 
                 prefix = 'val_' if not is_training_phase else ''
 
                 # Train over all batches
-                for i in range(n_batchs):
+                for _i in range(n_batchs):
                     if verbose > 1:
-                        print("-- %i / %i" % (i, n_batchs))
+                        print("-- %i / %i" % (_i, n_batchs))
 
                     batch_x, batch_y, _ = Trainer.get_batch(i, batch_size, _x, _y)
                     # Run optimization op (backprop), loss op (to get loss value)
                     # and summary nodes
-                    if i % 100 == 0:
+                    if _i == n_batchs - 1:
                         _ops = [summary_op, ]
                         _ops.extend(ops)
                         ret = sess.run(_ops, feed_dict={X: batch_x, Y_true: batch_y})
                         summary = ret[0]
-                        sv.summary_computed(sess, summary)
                         ops_loss_index = 2 if ret[1] is None else 1
                         # Write logs at every iteration
-                        _writer.add_summary(summary, epoch * n_batchs + i)
+                        _writer.add_summary(summary, epoch)
                     else:
                         ret = sess.run(ops, feed_dict={X: batch_x, Y_true: batch_y})
                         ops_loss_index = 1 if ret[0] is None else 0
 
                     loss_value = ret[ops_loss_index]
                     metrics_values = ret[ops_loss_index+1:]
+
+                    if verbose > 1:
+                        print("--- loss_value={} | metrics_values={}".format(loss_value, metrics_values))
+
+                    if np.isnan(loss_value):
+                        if verbose > 2:
+                            vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Model')
+                            ret = sess.run(vars)
+                            for r, v in zip(ret, vars):
+                                print("\n %s : \n" % v.name)
+                                print(r)
+                        raise Exception("Loss is nan")
 
                     # Compute average loss
                     avg_loss += loss_value * 1.0 / n_batchs
@@ -212,25 +226,29 @@ class Trainer:
             train_avg_metrics = None
             val_avg_loss = None
             val_avg_metrics = None
-            for epoch in range(training_epochs):
-                if sv.should_stop():
-                    break
+            try:
+                for epoch in range(training_epochs):
+                    if sv.should_stop():
+                        break
 
-                if verbose > 0 and (epoch + 1) % self.display_step == 0:
-                    print("Epoch: %04d" % (epoch + 1))
+                    if verbose > 0 and (epoch + 1) % self.display_step == 0:
+                        print("Epoch: %04d" % (epoch + 1))
 
-                train_avg_loss, train_avg_metrics = run_epoch(train_ops,
+                    train_avg_loss, train_avg_metrics = run_epoch(train_ops,
+                                                                  merged_summary_op,
+                                                                  train_x, train_y,
+                                                                  train_writer, is_training_phase=True)
+                    val_avg_loss, val_avg_metrics = run_epoch(val_ops,
                                                               merged_summary_op,
-                                                              train_x, train_y,
-                                                              train_writer, is_training_phase=True)
-                val_avg_loss, val_avg_metrics = run_epoch(val_ops,
-                                                          merged_summary_op,
-                                                          val_x, val_y,
-                                                          val_writer, is_training_phase=False)
+                                                              val_x, val_y,
+                                                              val_writer, is_training_phase=False)
 
-                if best_avg_val_loss is None or best_avg_val_loss > val_avg_loss + 0.005:
-                    best_avg_val_loss = val_avg_loss
-                    save_model(global_step, train_avg_loss, val_avg_loss)
+                    if best_avg_val_loss is None or best_avg_val_loss > val_avg_loss + 0.005:
+                        best_avg_val_loss = val_avg_loss
+                        save_model(global_step, train_avg_loss, val_avg_loss)
+            except Exception as e:
+                print("Stop optimization : ", e)
+                return
 
             if verbose > 0:
                 print("Optimization Finished!")
@@ -244,7 +262,7 @@ class Trainer:
         :param test_x:
         :param training_params:
         :param verbose:
-        :return:
+        :return: model predictions
         """
         assert isinstance(test_x, np.ndarray), "trainval_x should be an ndarray"
         Trainer._check_params(training_params)
@@ -256,8 +274,9 @@ class Trainer:
         n_features = np.prod(test_x.shape[1:])
 
         tf.reset_default_graph()
-        X = tf.placeholder(tf.float32, shape=(batch_size, n_features), name="X")
-        Y_pred = network_f(X)
+        X = tf.placeholder(tf.float32, shape=(None, n_features), name="X")
+        with tf.name_scope('Model'):
+            Y_pred = network_f(X)
 
         if verbose > 0:
             print("Start predictions")
@@ -267,42 +286,31 @@ class Trainer:
 
         # If there is a pretrained model
         pretrained_model_path = training_params['pretrained_model']
-        saver = tf.train.Saver()
+        if pretrained_model_path == 'best_from_logs':
+            pretrained_model_path = find_best_checkpoint(os.path.join(self._log_dir, "Model"))
 
-        tf.reset_default_graph()
+        saver = tf.train.Saver()
+        assert os.path.exists(pretrained_model_path + ".index"), \
+            "Model checkpoint '%s' is not found" % pretrained_model_path
+
         with tf.Session() as sess:
             saver.restore(sess, pretrained_model_path)
             n_samples = test_x.shape[0]
             y_pred = np.zeros((n_samples, 1))
-            n_batchs = int(n_samples / batch_size)
+            n_batchs = int(np.ceil(n_samples * 1.0 / batch_size))
 
             # Predict over all batches
             for i in range(n_batchs):
-                if verbose > 1:
-                    print("-- %i / %i" % (i, n_batchs))
+                if verbose > 0:
+                    print("-- %i / %i" % (i, n_batchs), end=' ')
 
                 batch_x, (start, end) = Trainer.get_batch(i, batch_size, test_x)
-
+                if verbose > 0:
+                    print(" | %i - %i" % (start, end))
                 ret = sess.run(Y_pred, feed_dict={X: batch_x})
-                y_pred[i]
+                y_pred[start:end, :] = ret
 
-                loss_value = ret[ops_loss_index]
-                metrics_values = ret[ops_loss_index + 1:]
-
-                # Compute average loss
-                avg_loss += loss_value * 1.0 / n_batchs
-                for j, v in enumerate(metrics_values):
-                    avg_metrics[j] += v * 1.0 / n_batchs
-
-                    # Display logs per epoch step
-            if verbose > 0 and (epoch + 1) % self.display_step == 0:
-                print(prefix + "loss=%.9f | " % avg_loss, end='')
-                for (name, _), avg_value in zip(metrics_list, avg_metrics):
-                    print(prefix + "%s=%.9f" % (name, avg_value), end=' ')
-                print('')
-
-            return avg_loss, avg_metrics
-
+            return y_pred
 
 
 def find_best_checkpoint(checkpoint_dir, field_name='val_loss', best_min=True):
